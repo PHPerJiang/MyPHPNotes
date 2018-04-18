fb74478fd4fad76518252b6696e13904434b8d9f
PHPerJiang <554246432@qq.com>
2018-04-18 08:31:20 +0800

    添加PHPMail扩展类库

---
+++ Phpmailer/extra/email.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *邮件相关配置 
+ */
+return [
+    'host'      => 'smtp.163.com',              
+    'username'  => 'a554246432@163.com',       
+    'password'  => 'jiangyu0613',
+    'setFrom'   =>  'a554246432@163.com',
+    'port'      =>  25,
+];
+
