5c042db6b8e373611249b0e476c74f2171dec920
PHPerJiang <554246432@qq.com>
2018-05-14 14:48:06 +0800

    添加了jquery的ajax请求用例

--- Ajax/Action/CreateJqueryAjaxRequest.php
+++ Ajax/Action/CreateJqueryAjaxRequest.php
@@ -1,6 +1,7 @@
 <?php
 //判断发来的请求类型用$_SERVER['REQUEST_METHOD'] && strtoupper($_SERVER['REQUEST_METHOD'])=='GET'
 if($_SERVER['REQUEST_METHOD'] && strtoupper($_SERVER['REQUEST_METHOD'])=='GET'){
+    //sleep(5);//5s后继续执行，模仿网络延迟
     $data=$_GET;
     echo $data['name'];
 }elseif ($_SERVER['REQUEST_METHOD'] && strtoupper($_SERVER['REQUEST_METHOD'])=='POST'){
