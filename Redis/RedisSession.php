2aa920095fd9c6eb1126de6053e81a080414796b
PHPerJiang <554246432@qq.com>
2018-06-22 15:16:13 +0800

    修改了RedisSession的应用实例，增加了销毁Redis存储的Session的方法详解

--- Redis/RedisSession.php
+++ Redis/RedisSession.php
@@ -13,6 +13,15 @@ $redis->connect('127.0.0.1',6379);
 //取出session
 echo json_encode($_SESSION['data']).'<br/>';
 
+//注销session的方式和将session存入服务器文件的方式一样的
+//session_destory();//销毁当前会话的所有数据包括当前会话
+//unset($_SESSION['data']);//注销会话指定内容，不销毁当前会话
+//$_SESSION['data']=null;//将指定数据设置为空，不推荐使用，不销毁当前会话
+/*当然php中也有配置设置的会话的过期时间session.gc_maxlifetime = 1440默认24分钟，
+ * redis默认过期时间未2147483647,一定要报正php.ini中的session的过期时间小于redis
+ * 中的过期时间不然会报错
+ */
+
 //打印sessionid
 echo 'session_id:'.session_id().'<br/>';
 //打印redis中session的值，注意session在reids的存储是以session_id的形式将session存入redis的
