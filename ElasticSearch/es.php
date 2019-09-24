295d2a242ba1ad91a17631cd6fc45e310de9c724
PHPerJiang <554246432@qq.com>
2019-09-24 11:47:21 +0800

    游标、聚合、脚本查询等

--- ElasticSearch/es.php
+++ ElasticSearch/es.php
@@ -401,4 +401,186 @@ class Es extends CI_Controller{
 		$client = ClientBuilder ::create() -> build();
 		echo json_encode($client -> search($params));
 	}
+
+	//函数式打分
+	function create_func_score_doc(){
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'id'    => 2,
+			'body'  => [
+				'test' => 'quick fox',
+				"popularity" => 5
+			]
+		];
+		$client = ClientBuilder ::create() -> build();
+		echo json_encode($client -> create($params));
+	}
+
+	/**
+	 * 使用打分函数来进行排序
+	 * @Author: jiangyu01
+	 * @Time: 2019/9/20 18:00
+	 */
+	function search_func_score(){
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'body'  => [
+				'query' => [
+					'function_score' => [
+						'query' => ['match' => ['test' => "quick brown fox"]],
+						'script_score' => [
+							'script' => [
+								'lang' => 'expression',
+								'source' => "_score + count",
+								'params' => ['count' => 2],
+							]
+						]
+					]
+				]
+			]
+		];
+		$client = ClientBuilder ::create() -> build();
+		echo json_encode($client -> search($params));
+	}
+
+	/**
+	 * 给一个字段打分
+	 * @Author: jiangyu01
+	 * @Time: 2019/9/21 14:34
+	 */
+	function search_script_field(){
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'body'  => [
+				'query' => [
+					'match_all' => new  \stdClass()
+				],
+				'script_fields' => [
+					'test1' => [            //脚本获取文章值并计算积
+						'script' => [
+							'lang' => 'painless',
+							'source' => "doc['popularity'].value * 2"
+						]
+					],
+					'test2' => [            //脚本获取参数值并与文档值乘积
+						'script' => [
+							'lang' => 'painless',
+							'source' => "doc['popularity'].value * params.count",
+							'params' => ['count' => 3]
+						]
+					],
+					'test3' => [               //给这个字段打分，但注意无法使用painless及paramas数组传值
+						'script' => "params['_source']['popularity'] * 4"
+					],
+				]
+			]
+		];
+		$client = ClientBuilder ::create() -> build();
+		echo json_encode($client -> search($params));
+	}
+
+	/**
+	 * 游标查询
+	 * @Author: jiangyu01
+	 * @Time: 2019/9/21 15:26
+	 */
+	public function search_by_scrolling(){
+		$client = ClientBuilder ::create() -> build();
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'scroll' => "30s",      //设置游标时间
+			'size'   => 1,          //设置每次查询数量
+			'body'  => ['query' => ['match_all' => new \stdClass()],
+			]
+		];
+		$response = $client->search($params);
+		$result = isset($response['hits']['hits']) ? $response['hits']['hits'] : [];    //缓存初次结果
+		while (isset($response['hits']['hits']) && count($response['hits']['hits']) > 0) {
+			$scroll_id = $response['_scroll_id'];
+			$response = $client->scroll([
+					"scroll_id" => $scroll_id,  // 使用上个请求获取到的  _scroll_id
+					"scroll" => "30s"           // 时间窗口保持一致
+				]
+			);
+			$result_tmp = isset($response['hits']['hits']) ? $response['hits']['hits'] : [];
+			$result = array_merge($result,$result_tmp);
+		}
+		var_dump($result);
+	}
+
+	/**
+	 *
+	 * @Author: jiangyu01
+	 * @Time: 2019/9/21 17:27
+	 */
+	function agg_search(){
+		$client = ClientBuilder ::create() -> build();
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'size'  => 0,
+			'body'  => [
+				'aggs' => [
+					'avg_popularity' => ['avg' => ['field' => 'popularity']],   //根据文档去除字段计算平均值
+					'avg_populartiy_by_script' => ['avg' => ['script' => ['source' => "doc.popularity.value * 2",]]],   //使用脚本计算平均值
+					'avg_def'        => ['avg' => ['field' => 'grade','missing' => 10]]   //文档中不存在的字段聚合结果是null,也可以指定确实字段值
+				],
+			]
+		];
+		$response = $client->search($params);
+		echo json_encode($response);
+	}
+
+	//单值、多值聚合
+	function agg_search_1(){
+		$client = ClientBuilder ::create() -> build();
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'size'  => 0,
+			'body'  => [
+				'aggs' => [
+					'min'   => ['min' => ['field' => 'popularity']],  //最小值聚合
+					'max'   => ['max' => ['field' => 'popularity']],  //最大值聚合
+					'avg'   => ['avg' => ['field' => 'popularity']],  //均值聚合
+					'sum'   => ['sum' => ['field' => 'popularity']],  //和值聚合
+					'cardinality' => ['cardinality' =>['field' => 'popularity']], //基数聚合，比如你文档中设置的性别有 男女两种，则基数为2
+					'stats' => ['stats' => ['field' => 'popularity']],  //基础度量，获取文档中此字段的基数、均值、最大、最小、和值
+					'extended_stats' => ['extended_stats' => ['field' => 'popularity']],  //额外度量聚合
+					'terms' => ['terms' => ['field' => 'popularity']],  //键值聚合，可以统计某个字段中每个键出现的次数
+					'value_count' => ['value_count' => ['script' => ['source' => "doc.value"]]]  //值统计，有几个值
+				],
+			]
+		];
+		$response = $client->search($params);
+		echo json_encode($response);
+	}
+
+	//聚合的排序
+	function agg_search_term_sort(){
+		$client = ClientBuilder ::create() -> build();
+		$params = [
+			'index' => 'func_score',
+			'type'  => 'doc',
+			'size'  => 0,
+			'body'  => [
+				'aggs' => [
+					#根据聚合后的term及响应聚合中的key进行排序只在histogram 和 date_histogram中使用,事实上也能在terms使用，也叫字典排序
+					'terms_by_key' => ['terms' => ['field' => 'popularity','order' => ['_key' => 'desc']]],
+					#根据聚合后响应中的doc_count进行排序，对terms\histogram\date_histogram中使用
+					'terms_by_count' => ['terms' => ['field' => 'popularity','order' => ['_count' => 'desc']]],
+					#根据词项的字符串的字母顺序排序，只在terms中使用，term在6.0中已经被废弃，如果使用成功是因为代码中使用了key来代替term
+					'terms_by_term' => ['terms' => ['field' => 'popularity','order' => ['_term' => 'desc']]]
+				],
+			]
+		];
+		$response = $client->search($params);
+		echo json_encode($response);
+	}
+
+
 }
